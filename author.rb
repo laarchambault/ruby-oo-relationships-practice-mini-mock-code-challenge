60f96f4d93f0ce8e077c8402ef4b88c2ec91e040
laarchambault <laarchambault@gmail.com>
2020-02-25 09:27:50 -0500

    finished testing

--- author.rb
+++ author.rb
@@ -1,1 +1,48 @@
+class Author
+    attr_accessor :name
+    @@all = []
 
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def books
+        this_auth_books = Book.all.select do |book|
+            book.author == self
+        end
+        #  for just book titles, not instances:
+        #  this_auth_books.map do |book|
+        #      book.title
+        #  end
+    end
+
+    def write_book(title, word_count)
+        Book.new(title, self, word_count)
+    end
+
+    def total_words
+        self.books.sum do |book|
+            book.word_count
+        end
+    end
+
+    def self.most_words
+        #look at hash of author with total words
+        #find max
+        wordy_author = nil
+        high_count = 0
+        self.all.each do |author_instance|
+            if author_instance.total_words > high_count
+                wordy_author = author_instance
+                high_count = author_instance.total_words
+            end
+        end
+        wordy_author
+    end
+
+end
--- console.rb
+++ console.rb
@@ -2,6 +2,18 @@ require 'pry'
 require_relative './book'
 require_relative './author'
 
+#Authors
+mary = Author.new("Mary Fluffkins")
+johann = Author.new("Johann Marize")
+corben = Author.new("Corben B")
+tasha = Author.new("Tasha Jones")
+
+unicorns = Book.new("Unicorn Book", mary, 3982)
+theology = Book.new("Theology Book", tasha, 523)
+recipe = Book.new("Recipe Book", mary, 109)
+action = Book.new("Action Book", corben, 323)
+romance = Book.new("Romance Book", johann, 303)
+
 binding.pry
 
 
